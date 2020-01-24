4cc21ac663e33997dfb7307b785d630b705b174d
Alex <AlexanderDesmond848898@gmail.com>
2020-01-24 16:08:32 +1100

    Manually add cards.json file to assets folder.

--- gwent-chest-app/src/app/app.component.ts
+++ gwent-chest-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'gwent-chest-app';
+  title = "gwent-chest-app";
 }
